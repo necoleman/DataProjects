3a5225bfd76c8a61e12cdd2c0c7756de0e667bc7
necoleman <coleman.neal@gmail.com>
2016-02-09 08:44:09 -0500

    Changes to mnist and neural net. Strange bug happening -- it learns *too well.*

--- MNIST/mnist.py
+++ MNIST/mnist.py
@@ -1,32 +1,47 @@
 import numpy as np
 from sklearn import linear_model
+import neural_net
 import os
 
+# These routines load the MNIST data and learn various algorithms on it
+# They then let you save the predictions to a csv file in the appropriate
+# format to upload to Kaggle.
+
+# Loads data from the csv files provided by Kaggle
 def loadData():
     raw_train_data = np.loadtxt( 'train.csv', dtype='string',delimiter=',' )[1:,:].astype(np.float)
     raw_test_data = np.loadtxt( 'test.csv', dtype='string',delimiter=',' )[1:,:].astype(np.float)
     #print raw_train_data[:,:3]
     return raw_train_data, raw_test_data
 
+# Normalizes data so the input vectors lie between 0 and 1.
 def Normalize(training_data):
     return training_data / 255.
 
-def formatTrainingData(training_data):
+# Formats the training data: Splits the loaded data into
+# the features and the labels, then rewrites each label
+# as a basis vector, e.g., 3 ---> [0,0,0,1,0,0,0,0,0,0]
+def formatLabelToVector(training_data):
     # take the loaded training_data and split it into
     # the pixel data and the labels
-
     # set up the image classification vector
     img_id = training_data[:,0]
-    pixel_data = training_data[:,1:]
+    pixel_data = list(training_data[:,1:])
     img_vec = []
     for k in range(img_id.shape[0]):
         vec = np.zeros(10)
         vec[int(img_id[k])] = 1.
         img_vec.append(vec)
     img_vec = np.array(img_vec)
-    return pixel_data,img_vec
+    for j in pixel_data:
+        j = Normalize(np.array(j))
 
-def formatData(data):
+    return zip(pixel_data,img_vec)
+
+# Formats the predictions
+# reinterprets the basis vector as a label
+# e.g. [0,0,0,1,0,0,0,0,0,0] ---> 3
+def formatVectorToLabel(data):
     # format predicted data
     # from a max-values-predicted to a list of labels
     # (conversion: index of max prediction is the label)
@@ -37,6 +52,19 @@ def formatData(data):
     submission = np.array(submission)
     return submission   
 
+# Writes the predicted, assumed formatted, data to the predictions.csv file
+def write(data):
+    # print predictions to csv
+    #os.remove('predictions.csv')
+    with open('predictions.csv', 'a+') as f:
+        f.write('ImageId,Label\n')
+        it = 1
+        for j in data:
+            f.write(str(it)+','+str(int(j))+'\n')
+            it+=1
+
+# Runs a straight-up honest-to-god linear regression on the training and test data
+# Returns a prediction on the test data
 def NaiveRegression(training_data,test_data):
     pixel_data,img_vec = formatTrainingData(training_data)
     ols = linear_model.LinearRegression()
@@ -44,39 +72,50 @@ def NaiveRegression(training_data,test_data):
     prediction = ols.predict(test_data)
     return formatData(prediction)
 
+# Runs a SGD on the data and returns a prediction
 def StochasticGradientDescent(pixel_data,img_labels,test_data):
     classifier = linear_model.SGDClassifier()
     classifier.fit(pixel_data,img_labels)
     prediction = classifier.predict(test_data)
     return prediction
-    
+
+# Runs a Perceptron on the test data and returns a prediction    
 def Perceptron(pixel_data,img_labels,test_data):
     p = linear_model.Perceptron()
     p.fit(pixel_data,img_labels)
     return p.predict(test_data)
 
+# Someday this may turn into a K-clustering algorithm
+# with 10 clusters
 def KNeighbors():
     return
 
+# Runs a log regression
+# (Rather, tries to. It doesn't converge at the moment, for some reason.)
 def LogRegression(pixel_data,img_vec,test_data):
     logreg = linear_model.LogisticRegression(max_iter=1000,solver='sag',verbose=1)
     logreg.fit(pixel_data,img_vec)
     raw_pred = logreg.predict(test_data)
     return raw_pred
 
-def write(data):
-    # print predictions to csv
-    #os.remove('predictions.csv')
-    with open('predictions1.csv', 'a+') as f:
-        f.write('ImageId,Label\n')
-        it = 1
-        for j in data:
-            f.write(str(it)+','+str(int(j))+'\n')
-            it+=1
-
+# Algorithm of the day: Homebrew neural net!
 if __name__ == '__main__':
     train,test = loadData()
-    pixel_data,img_labels = train[:,1:],train[:,0]
-    pixel_data = Normalize(pixel_data)
-    prediction = Perceptron(pixel_data,img_labels,test)
-    write(prediction)
+    training_data = formatLabelToVector(train)
+    pixel_length = training_data[0][0].shape[0]
+    N = neural_net.Network(3,pixel_length,10,0.01)
+    print N.profile()
+
+
+    print "     Now training ..."
+    for j in range(5):
+        N.train(training_data[:500],1)
+    print "     Done training! Now validating ..."
+
+    print N.validate(training_data[500:510])
+
+    test_list = list(test)
+    predictions = [ N.predict( np.array(j) ) for j in test_list ]
+    predictions = formatVectorToLabel( np.array(predictions) )
+    write(predictions)
+    
--- MNIST/neural_net.py
+++ MNIST/neural_net.py
@@ -1,8 +1,16 @@
 import numpy as np
+import sys
 
 # Here are some helper functions for what follows
 def Sigmoid(t):
-    return 1./(1.+np.exp(-t))
+    x = 0.5
+    if t < -40.:
+        x = 0.0
+    elif t > 40.:
+        x = 1.0
+    else:
+        x = 1./(1.+np.exp(-t))
+    return x
 
 def DSigmoid(t):
     return Sigmoid(t)*(1-Sigmoid(t))
@@ -10,6 +18,8 @@ def DSigmoid(t):
 def DCost(t, outputs, trues):
     return (outputs - trues)
 
+# Separating "activation function" from sigmoid
+# To more easily allow for a different activation function
 def ActivationFunction(t):
     return Sigmoid(t)
 
@@ -20,7 +30,7 @@ def InnerProduct(u,v):
     # take in two 1D arrays of the same shape
     U = np.reshape( u, (1,u.shape[0]) )
     V = np.reshape( v, (v.shape[0],1) )
-    return U.dot(V)
+    return (U.dot(V)).flatten()[0]
 
 def OuterProduct(u,v):
     # take in two arbitrary 1D arrays
@@ -47,13 +57,15 @@ class Neuron:
     # the neuron class knows itself, knows its inputs, and produces an output
     def __init__(self,weights,epsilon):
         self.weights = weights
-        self.bias = 1.
-        self.input_size = weights.shape
+        self.bias = 0.
+        self.input_size = weights.shape[0]
         self.learning_rate = epsilon
+        #print self.input_size
         return
 
     def adjustWeights(self,inputs,gradw,gradb):
         self.weights -= self.learning_rate * gradw
+        self.weights / np.sum(self.weights)
         self.bias -= self.learning_rate * gradb
         return
 
@@ -64,19 +76,27 @@ class Layer:
     # this class comprises a layer of neurons and handles both input/output
     # and backpropagation
     def __init__(self,num_inputs,num_outputs,learning_rate, is_last):
-        self.neuron_list = [Neuron(RandomWeights(num_inputs),learning_rate) for k in range(num_outputs)]
+        #print " Creating " + str(num_outputs) + " neurons!"
+        self.neuron_list = [Neuron(np.ones(num_inputs)/num_inputs,learning_rate) for k in range(num_outputs)]
         self.last_output = np.zeros(num_outputs)
         self.last_input = np.zeros(num_inputs)
         self.is_last = is_last
         self.learning_rate = learning_rate
         return
+
+    def __len__(self):
+        return len(self.neuron_list)
     
     def weightArray(self):
         return np.array( [n.weights for n in self.neuron_list] )
 
     def fire(self,inputs):
+        #print '     predicting!'
         self.last_input = inputs
-        self.last_output = np.array([n.fire(inputs) for n in self.neuron_list])
+        #print self.last_input
+        self.last_output = np.reshape(np.array([n.fire(inputs) for n in self.neuron_list]),len(self.neuron_list))
+        #print "         "+str(self.last_input.shape)
+        #print "         "+str(self.last_output.shape)
         return self.last_output
 
     # perform backpropagation. return the critical values to calculate the error for the 
@@ -84,16 +104,20 @@ class Layer:
     # c.f. "the four key backpropagation equations" found here:
     # http://neuralnetworksanddeeplearning.com/chap2.html
     def backPropagate(self,next_errors):
-        #print next_errors.shape
+        #print '         backpropagating through a layer ...'
         #next_errors = np.reshape( next_errors, (1,next_errors.shape[0]) )
         this_errors = next_errors * self.last_output
+        if np.isnan(this_errors).any():
+            print next_errors
+            print self.last_output
+            sys.exit()
         gradb = this_errors
-        gradw = (self.last_input[np.newaxis].T).dot(this_errors[np.newaxis])
+        #print self.last_input.shape
+        #print this_errors.shape
+        gradw = OuterProduct(self.last_input,this_errors)
         error_to_hand_up = self.weightArray().T.dot(this_errors.T)
         for n in self.neuron_list:
             i = self.neuron_list.index(n)
-            #print gradw.shape
-            #print gradb.shape
             n.adjustWeights(self.last_input,gradw[:,i],gradb[i])
         return error_to_hand_up
         
@@ -109,25 +133,54 @@ class Network:
         return
 
     def assembleNetwork(self):
-        # network profile:
-        # each hidden layer has (five?) neurons
-        intermediate_num = 5
-        initial_layer = Layer(self.input_size,intermediate_num,self.learning_rate,False)
+        print self.input_size
+        initial_layer = Layer(self.input_size,self.input_size,self.learning_rate,False)
+        intermediate_num = (self.input_size + self.output_size)/2
+        intermediate_layer = Layer(self.input_size,intermediate_num,self.learning_rate,False)
         final_layer = Layer(intermediate_num,self.output_size,self.learning_rate,True)
-        layers = [initial_layer]
-        for k in range(self.num_layers - 1):
-            layers.append(Layer(intermediate_num,intermediate_num,self.learning_rate,False))
-        layers.append(final_layer)
+        layers = [initial_layer, intermediate_layer, final_layer]
         return layers
+
+    def profile(self):
+        return [len(l) for l in self.layers]
 
     def predict(self,inputs):
         prev_input = inputs
         for l in self.layers:
-            print l
+            #print "predicting in the " + str(l) + " layer"
             prev_input = l.fire(prev_input)
-        return prev_input
+        return np.array(prev_input).reshape(self.output_size)
+
+    # checks an individual input
+    def checkPrediction(self,x,y):
+        y_ = self.predict(x)
+        return (np.argmax(y) == np.argmax(y_))
+
+    def predictLabel(self,x):
+        return np.argmax(self.predict(x))
+
+    def stringValidation(self,x,y):
+        y_ = self.predict(x)
+        return str(np.argmax(y)) + ' = ' + str(np.argmax(y_))
+
+    # checks a bunch of validation inputs for correctness
+    def validate(self,validation_data):
+        result_list = []
+        for train,label in validation_data:
+            if( self.checkPrediction(train,label) ):
+                result_list.append(1.0)
+            else:
+                result_list.append(0.0)
+        result_list = np.array(result_list)
+
+        strlist = []
+        for train,label in validation_data:
+            strlist.append(self.stringValidation(train,label))
+        print strlist
+        return np.average(result_list)
 
     def backPropagate(self,error):
+        #print "     backpropagating!"
         hand_back = error
         self.layers.reverse()
         for l in self.layers:
@@ -135,17 +188,32 @@ class Network:
         self.layers.reverse()
         return
 
-    def trainIndividually(self,trainer,label):
-        for it in range(1000):
-            print '\r'+str(it)
-            self.backPropagate( self.predict(trainer) - label )
+    def trainIndividually(self,trainer,label,num_it=0):
+        #print "Training individually ..."
+        X = self.predict(trainer)
+        Y = label
+        if num_it == 0:
+            while( not self.checkPrediction(trainer,label) ):
+                print "         predict: " + str(self.predictLabel(trainer))
+                self.backPropagate( X - Y )
+                X = self.predict(trainer)
+                Y = label
+                #print str(np.argmax(X)) + " = " + str(np.argmax(Y)) + "? " + "not there yet ..."
+        else:
+            for k in range(num_it):
+                self.backPropagate( X-Y )
+                X = self.predict(trainer)
+                Y = label
+        X = np.argmax(self.predict(trainer))
+        Y = np.argmax(label)
+        print str(X) + ' = ' + str(Y) + ': ' + str(np.argmax(self.predict(trainer)) == np.argmax(label) )
         return
 
-    # trains the neural network
+    # trains the neural network with num_it iterations per training datum
     # (assumes that data are in this format:
     # { (vector,label) }
-    def train(self,training_data):
+    def train(self,training_data,num_it):
         for trainer,label in training_data:
-            print "NEW TRAINING DATA"
-            self.trainIndividually(trainer,label)
+            #print "NEW TRAINING DATA"
+            self.trainIndividually(trainer,label,num_it)
         return
